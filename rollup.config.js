0f6cd1560a58b112eeec2e1dee1e4817c3c4b3c1
Łukasz Holeczek <lukasz@holeczek.pl>
2021-08-11 21:22:12 +0200

    build: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
-import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
 import pkg from './package.json'
+
 export default {
   input: 'src/index.ts',
   output: [
@@ -23,9 +25,7 @@ export default {
     external(),
     resolve(),
     typescript({
-      rollupCommonJSResolveHack: true,
       exclude: ['**/__tests__/**'],
-      clean: true,
     }),
     commonjs({
       include: ['node_modules/**'],
